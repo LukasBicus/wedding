e263c025a74d3f22442e8215146051fda3a57053
lukasbicus <ing.lukas.bicus@gmail.com>
2019-05-15 15:08:55 +0200

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Janka a Lukáš`,
+    description: `Rozhodli sme sa vykročiť na spoločnú cestu životom`,
+    author: `@lukasbicus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
