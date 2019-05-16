29c143de02d82d0934ebffc5db96684e44834e40
lukasbicus <ing.lukas.bicus@gmail.com>
2019-05-16 11:01:40 +0200

    Add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+const H1 = styled.h1`
+  background: green;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
+    <H1>Hi people</H1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem', background: 'red' }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
