623ddd6980bbcd4a56cbe039934e108c31272591
lukasbicus <ing.lukas.bicus@gmail.com>
2019-10-12 11:50:48 +0200

    Add package for styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,10 @@ module.exports = {
         path: path.join(__dirname, 'src', 'data')
       }
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    'gatsby-plugin-styled-components',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ]
 }
