729d853bf15144e281b99a8491184ddafb7ea0ff
lukasbicus <ing.lukas.bicus@gmail.com>
2019-10-20 01:07:58 +0200

    Update SEO

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -44,6 +44,7 @@ function Seo({ description, lang, meta, title }) {
     `
   )
   const metaDescription = description || aboutSite.description
+  console.log(`metaDes: ${metaDescription}`)
   return (
     <Helmet
       htmlAttributes={{
@@ -51,54 +52,28 @@ function Seo({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${aboutSite.title}`}
-      meta={[
-        {
-          name: 'description',
-          content: metaDescription
-        },
-        {
-          property: 'og:title',
-          content: title
-        },
-        {
-          property: 'og:description',
-          content: metaDescription
-        },
-        {
-          property: 'og:type',
-          content: 'website'
-        },
-        {
-          name: 'twitter:card',
-          content: 'summary'
-        },
-        {
-          name: 'twitter:creator',
-          content: aboutSite.author
-        },
-        {
-          name: 'twitter:title',
-          content: title
-        },
-        {
-          name: 'twitter:description',
-          content: metaDescription
-        }
-      ]
-        .concat(
-          seoKeywords.length > 0
-            ? {
-              name: 'keywords',
-              content: seoKeywords.join(', ')
-            }
-            : []
-        )
-        .concat(meta)}
+      meta={
+        (seoKeywords.length > 0
+          ? [{
+            name: 'keywords',
+            content: seoKeywords.join(', ')
+          }]
+          : []
+        ).concat(meta)}
     >
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
       />
+      <meta name="Description" content={metaDescription} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content={'website'} />
+      <meta name='twitter:card' content={'summary'} />
+      <meta name='twitter:creator' content={aboutSite.author} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={metaDescription} />
       <link
         href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap:200,400"
         rel="stylesheet"
